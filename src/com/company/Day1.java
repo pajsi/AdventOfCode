191a8b2f05e1cecd2dd1e3aece2a0ae47e413650
pajsi <maciej.pajsert@gmail.com>
2021-12-28 16:51:09 +0100

    Initial commit

---
+++ src/com/company/Day1.java
@@ -0,0 +1,90 @@
+// Advent od Code 2021
+// Day 1: Sonar Sweep
+//https://adventofcode.com/2021/day/1#
+
+package com.company;
+
+// It imports all io methods in one
+//import java.io.*;
+
+// But nice to know used methods
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1
+{
+    public static void calculate()
+    {
+        int numberOfLines = 0;
+        int sizeOfArray = 2000;
+
+        // declares an array of integers
+        int[] InputLinesInArray;
+        // allocates memory for array
+        InputLinesInArray = new int[sizeOfArray];
+
+        // File path is passed as parameter
+        // Note:  Double back quote is to avoid compiler interpret words like \test as \t (ie as an escape sequence)
+        File file = new File("C:\\Users\\Pajs\\IdeaProjects\\AdventOfCode\\src\\com\\company\\input_Day1.txt");
+
+        try
+        {
+            // Creating an object of BufferReader class
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            //Declaring a string variable
+            String st;
+
+            // Condition holds true till there is character in a string
+            while ((st = br.readLine()) != null)
+            {
+                // convert string from read file line into int with error handling
+                try
+                {
+
+                    //convert string to integer
+                    int number = Integer.parseInt(st);
+
+                    // save each line in array
+                    InputLinesInArray[numberOfLines] = number;
+
+                    //print current line
+                    System.out.println(InputLinesInArray[numberOfLines]);
+
+                }
+                // try {} catch {} for handling exceptions if string from read file line is not numeric
+                catch (NumberFormatException ex) {ex.printStackTrace();}
+
+                numberOfLines++;
+            }
+
+            // close opened buffer reader
+            br.close();
+
+            // print the number of lines in a file
+            System.out.println("Number of lines in a file: " + numberOfLines);
+
+        }
+        // try {} catch {} for handling exceptions if file doses not exists
+        catch (FileNotFoundException e) { System.err.println("File does not exist"); }
+        catch (IOException e) { System.err.println("File exists, but there was IOException"); }
+
+        int increased = 0;
+        int decreased = 0;
+        int staysTheSame = 0;
+
+        for (int i = 1; i < sizeOfArray; i++)
+        {
+            if (InputLinesInArray[i] > InputLinesInArray[i-1]) { increased++; }
+            else if (InputLinesInArray[i] < InputLinesInArray[i-1]) { decreased++; }
+            else  { staysTheSame++; }
+        }
+
+        System.out.println("Number of increased: " + increased);
+        System.out.println("Number of decreased: " + decreased);
+        System.out.println("Number of staysTheSame: " + staysTheSame);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+// Advent od Code 2021
+//https://adventofcode.com/2021/
+
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Day1 day1 = new Day1();
+        day1.calculate();
+
+    }
+}
